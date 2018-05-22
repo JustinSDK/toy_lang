8aa890655c17e04a2a8f14ffd6bc3e385d8bdbb6
Justin Lin <caterpillar@openhome.cc>
2018-05-22 15:17:23 +0800

    add built-in String class

--- js/builtins.js
+++ js/builtins.js
@@ -1,5 +1,5 @@
-import {Primitive, Func} from './ast/value.js';
-import {Variable} from './ast/statement.js';
+import {Primitive, Func, Class, Instance} from './ast/value.js';
+import {Variable, StmtSequence, VariableAssign} from './ast/statement.js';
 
 export {BUILTINS};
 
@@ -14,19 +14,55 @@ class Print {
     }
 }
 
-class Len {
-    constructor(value) {
-        this.value = value;
-    }
+const ONE_PARAM = new Variable('v');
 
-    evaluate(context) {
-        return context.returned(new Primitive(this.value.evaluate(context).value.length));
-    }    
+function classBodyStmt(initFunc) {
+    return new StmtSequence(
+        new VariableAssign(new Variable('init'), initFunc),
+        StmtSequence.EMPTY
+    );
 }
 
-const ONE_PARAM = new Variable('v');
+const Str = new Map([
+    ['init', new Func([ONE_PARAM], {
+        evaluate(context) {
+            let properties = new Map([
+                ['value', ONE_PARAM.evaluate(context)],
+                ['length', new Primitive(ONE_PARAM.evaluate(context).value.length)]
+            ].concat(Array.from(Str.entries())));
+            return context.assign('this', new Instance(properties));
+        }
+    })],
+    ['charAt', new Func([ONE_PARAM], {
+        evaluate(context) {
+            let instance = context.variables.get('this');
+            let text = instance.properties.get('value').value;
+            return context.returned(
+                new Primitive(text.charAt(ONE_PARAM.evaluate(context).value))
+            );
+        }    
+    })],
+    ['toUpperCase', new Func([], {
+        evaluate(context) {
+            let instance = context.variables.get('this');
+            let text = instance.properties.get('value').value;
+            return context.returned(
+                new Primitive(text.toUpperCase())
+            );
+        }    
+    })],   
+    ['toLowerCase', new Func([], {
+        evaluate(context) {
+            let instance = context.variables.get('this');
+            let text = instance.properties.get('value').value;
+            return context.returned(
+                new Primitive(text.toLowerCase())
+            );
+        }    
+    })], 
+]);
 
 const BUILTINS = new Map([
     ['print', new Func([ONE_PARAM], new Print(ONE_PARAM))],
-    ['len', new Func([ONE_PARAM], new Len(ONE_PARAM))]
+    ['String', new Class([], classBodyStmt(Str.get('init')))]
 ]);
