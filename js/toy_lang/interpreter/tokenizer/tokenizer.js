186deab2f3ee73b2799305cece0cf2d368aa000d
Justin Lin <caterpillar@openhome.cc>
2018-06-18 15:02:36 +0800

    fix comment pattern

--- js/toy_lang/interpreter/tokenizer/tokenizer.js
+++ js/toy_lang/interpreter/tokenizer/tokenizer.js
@@ -155,7 +155,7 @@ class Tokenable {
 class Tokenizer {
     constructor(code) {
         this.lines = code.trim().split('\n')
-                        .map(line => line.replace(/#.*/, '')) // A comment starts with #
+                        .map(line => line.replace(/(('(.*)#(.*)'[^#]*)*)(#.*)?$/, '$1')) // A comment starts with #
                         .map(line => line.trim())
                         .map((line, idx) => new Tokenable('line', idx + 1, line))
                         .filter(tokenizableLine => tokenizableLine.value !== '' && !tokenizableLine.value.startsWith("#")); 
