8cb28e5f800e1076b478b3579cfac1219c64ae38
Justin Lin <caterpillar@openhome.cc>
2018-07-02 11:14:06 +0800

    global module

--- js/toy_lang/builtin/classes/module.js
+++ js/toy_lang/builtin/classes/module.js
@@ -1,5 +1,24 @@
+import {Primitive} from './ast_export.js';
+
+import {func0} from '../bases/func_bases.js';
+import {self, selfInternalNode} from '../bases/class_bases.js';
+
 export {ModuleClass};
 
 class ModuleClass {}
 
-ModuleClass.methods = new Map();
+ModuleClass.methods = new Map([
+    ['name', func0('name', {
+        evaluate(context) {
+            const ctxNode = selfInternalNode(context);
+            return context.returned(new Primitive(ctxNode.fileName.replace('.toy', '')));
+        }    
+    })],
+    ['toString', func0('toString', {
+        evaluate(context) {
+            const clzNode = self(context).clzNodeOfLang();
+            const ctxNode = selfInternalNode(context);
+            return context.returned(new Primitive(`[${clzNode.name} ${ctxNode.fileName.replace('.toy', '')}]`));
+        }    
+    })]
+]);
