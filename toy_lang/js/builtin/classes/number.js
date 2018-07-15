45a4554bf854d101e4a8401facd8f5737f532a9e
Justin Lin <caterpillar@openhome.cc>
2018-07-15 14:02:33 +0800

    change constants setting loc

--- toy_lang/js/builtin/classes/number.js
+++ toy_lang/js/builtin/classes/number.js
@@ -8,6 +8,8 @@ NumberClass.methods = new Map();
 NumberClass.constants = new Map([
     ['MAX_VALUE', Primitive.of(Number.MAX_VALUE)],
     ['MIN_VALUE', Primitive.of(Number.MIN_VALUE)],
+    ['MAX_SAFE_INTEGER', Primitive.of(Number.MAX_SAFE_INTEGER)],
+    ['MIN_SAFE_INTEGER', Primitive.of(Number.MIN_SAFE_INTEGER)],
     ['NaN', Primitive.of(Number.NaN)],
     ['POSITIVE_INFINITY', Primitive.of(Number.POSITIVE_INFINITY)],
     ['NEGATIVE_INFINITY', Primitive.of(Number.NEGATIVE_INFINITY)]
